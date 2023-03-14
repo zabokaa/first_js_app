2e96eb7d2512247be2e5bf4a9dc526546c1b4ef2
zabokaa <>
2023-03-14 15:27:46 +0100

    Created array var 'pokemonList'; Renamed page

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,12 @@
-alert("go to bed");
-let favFood = "ceviche";
-document.write(favFood);
+// var arrow pokemonList
+// name, height, types '' look at pdoex.org
+
+let pokemonList = [
+    {name: "venonant", height: 1.0, types: ["bug", "poison"],speed: 45},
+    {name: "raticate", height: 0.7, types: "normal", speed: 97},
+    {name: "tentacruel", height: 1.6, types: ["water", "posion"], speed: 100},
+    {name: "voltorb", height: 0.5, types: "electric", speed: 100},
+    {name: "spinda", height: 1.1, types: " normal", speed: 60}
+];
+
+console.log(pokemonList[2]);
