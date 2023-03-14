edaed42f5c98fe93452f2348f70eaa5a12f413ce
zabokaa <>
2023-03-14 21:37:44 +0100

    Included 1st doc.write

--- scripts/script.js
+++ scripts/script.js
@@ -4,7 +4,26 @@
 let pokemonList = [
     {name: "venonant", height: 1.0, types: ["bug", "poison"],speed: 45},
     {name: "raticate", height: 0.7, types: "normal", speed: 97},
-    {name: "tentacruel", height: 1.6, types: ["water", "posion"], speed: 100},
+    {name: "tentacruel", height: 1.6, types: ["water", "poison"], speed: 100},
     {name: "voltorb", height: 0.5, types: "electric", speed: 100},
     {name: "spinda", height: 1.1, types: " normal", speed: 60}
 ];
+document.write(pokemonList[3].name);
+
+/*Create for loop and inside it add a conditional. 
+The conditional should check if the height is above a certain value (you’re free to pick whatever value you want). 
+// If it is, add the note “Wow, that’s big!” to the output. */
+// for (let i=0; i<=pokemonList.len; i++){
+//     if (pokemonList[i].speed >= 60){
+//         document.write("Wow, you are super fast!");
+//     }
+// };
+
+// for (let i=1; i<=pokemonList.len; i++) {
+//     if (pokemonList[i].speed >= 60) && (pokemonList[i].types == "posion"){
+//         document.write(pokemonList[i].name + " is speedy and poison! Wow, best skills!");
+//     }
+// }
+
+
+
