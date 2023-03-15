005c68d509204ed60dc00fcf828e298ee10991a9
zabokaa <>
2023-03-15 14:57:47 +0100

    Added 2nd condition

--- scripts/script.js
+++ scripts/script.js
@@ -14,14 +14,13 @@ let pokemonList = [
 The conditional should check if the height is above a certain value (you’re free to pick whatever value you want). 
 // If it is, add the note “Wow, that’s big!” to the output. */
 
-// 2nd condition && (pokemonList[i].types === "posion")
 
 for (let i = 0; i < pokemonList.length; i++) {
-    if (pokemonList[i].speed >= 100) {
-        document.write("wow, " + pokemonList[i].name + " is speedy" + " (speed: " + pokemonList[i].speed + ")! // ");
+    if ((pokemonList[i].speed >= 70) && (pokemonList[i].types === "normal")) {
+        document.write("wow, " + pokemonList[i].name + " is speedy" + " (speed: " + pokemonList[i].speed + ") and poisonous! // ");
     }
-    else {document.write(pokemonList[i].name + " (speed: " + pokemonList[i].speed + ") // "      
-      );
+    else {
+        document.write(pokemonList[i].name + " (speed: " + pokemonList[i].speed + ") // ");
     }
 }
 
